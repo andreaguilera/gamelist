d4b908459d2947ebbcdc6d596b6d3745a67b4659
andreaguilera <andre.aguilera55@hotmail.com>
2022-09-06 10:55:36 -0300

    frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./assets/logo-g.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
