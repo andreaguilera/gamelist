1aecb87801b41399bdd86fea227b5794d6dd4074
andreaguilera <andre.aguilera55@hotmail.com>
2022-09-21 09:15:39 -0300

    integrate playing tab to api

--- frontend/src/components/AllTabs/PlayingTab.js
+++ frontend/src/components/AllTabs/PlayingTab.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 
 const PlayingTab = () => {
+  const [playingGames, setPlayingGames] = useState([]);
+
+  useEffect(() => {
+    const fetchPlayingGames = async () => {
+      try {
+        const response = await api.get("/game/Playing");
+        setPlayingGames(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchPlayingGames();
+  }, []);
   return (
     <div className="PlayingTab">
-      <p>play Tab!! Hurray!!</p>
-      {/* First tab content will go here */}
+      {playingGames.map((game) => (
+        <li key={game.id}>{game.name}</li>
+      ))}
     </div>
   );
 };
